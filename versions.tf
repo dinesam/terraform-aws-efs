9e2160e423a92fd95723e69920ca769b0c6526f4
dinesam <dinesam@gmail.com>
2024-12-06 15:38:08 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.10.0"
+  required_version = ">= 1.8.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.80"
+      version = ">= 5.0"
     }
   }
 }
